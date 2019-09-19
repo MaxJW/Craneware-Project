4886b009e0ac1b1753ea32bf236f2ba85141a1af
Lucy Taylor <lucy.taylor@protonmail.com>
2019-09-19 15:47:04 +0100

    Fixed Sorting

--- src/app/tableview/tableview.component.ts
+++ src/app/tableview/tableview.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material';
+import {MatTableDataSource} from '@angular/material/table';
 
 export interface Condition {
   code: number,
@@ -7,7 +9,7 @@ export interface Condition {
 }
 
 const CONDITION_DATA: Condition[] = [
-  {code: 6294, condition: 'Broken leg', hospital: 'Random'},
+  {code: 6293, condition: 'Fractured Tibia', hospital: 'Cerys Memorial Hospital'},
   {code: 6294, condition: 'Broken leg', hospital: 'Random'},
   {code: 6294, condition: 'Broken leg', hospital: 'Random'},
 ];
@@ -17,7 +19,9 @@ const CONDITION_DATA: Condition[] = [
   templateUrl: './tableview.component.html',
   styleUrls: ['./tableview.component.css']
 })
-export class TableviewComponent {
+export class TableviewComponent implements OnInit {
   displayedColumns: string[] = ['code', 'condition', 'hospital'];
-  dataSource = CONDITION_DATA;
+  dataSource = new MatTableDataSource(CONDITION_DATA);
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  ngOnInit(){this.dataSource.sort = this.sort;}
 }
