196d33ac852ae3a1f082feb1bf9d6ab131a0428c
Sebastian6656 <46683455+Sebastian6656@users.noreply.github.com>
2019-09-20 09:39:10 +0100

    Updated background images to use medical related images, subject to change. Created data service that is used to update and manage data responce data and table data within the application. Updated Http Service to wait for responce before displaying results and also extended functionality to work with the data service. Modified search form component to comment out temporary table that was in use for testing. added in await and commented out old system for testing displaying responces in the searchForm. Updated tableview component with more descriptive table headings. Updates tableview ts to work with the data service and use the data from the request in the table, updating with every request.

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
+import { DataService } from './data.service';
+
 @Injectable({providedIn: 'root'})
 export class HttpService{
   searchDrgUrl: string = 'http://104.248.165.91:8000/api/searchDRG';
   getbaseUrl: string = 'http://104.248.165.91:8000/';
 
-  constructor(private http: HttpClient) { }
+  public responce: any;
+
+  constructor(private http: HttpClient, public dataService: DataService) { }
 
   sendPostRequest(data: string) {
     console.log("sending req")
@@ -21,13 +25,13 @@ export class HttpService{
     });
 
     return this.http
-      .post(this.searchDrgUrl, body, { headers })
-      .subscribe(res => console.log(res));
+      .post(this.searchDrgUrl, body, { headers }).toPromise().then(res => { this.responce = res, this.dataService.setSearchData(this.responce); } );
+      //.subscribe(res => this.responce = res);
   }
 
   sendGetRequest() {
     return this.http
-      .get(this.getbaseUrl)
-      .subscribe(res => console.log(res));
+      .get(this.getbaseUrl).toPromise().then(res => console.log(res));
+      //.subscribe(res => console.log(res));
   }
 }
