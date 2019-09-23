07c98d329db5ffe672c1fc0ff0582498d3e00305
Lucy Taylor <lucy.taylor@protonmail.com>
2019-09-23 15:57:37 +0100

    ↑↑↓↓←→←→🄱🄰⏎

---
+++ app.component.ts
@@ -0,0 +1,51 @@
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { trigger, state, transition, animate, style } from '@angular/animations';
+import { ShortcutInput, ShortcutEventOutput } from 'ng-keyboard-shortcuts';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('slideIn', [
+      transition(':enter', [
+        style({transform: 'translateY(100%)'}),
+        animate('700ms ease-in', style({transform: 'translateY(0%)'}))
+      ])
+    ]),
+    trigger('slideAdjust', [
+      transition('false=>true', [
+        style({transform: 'translateY(200%)'}),
+        animate('60ms ease-in', style({transform: 'translateY(0%)'}))
+      ])
+    ]),
+  ]
+})
+export class AppComponent implements AfterViewInit{
+  hasSearched: boolean = false;
+  mapSelect: any;
+  shortcuts: ShortcutInput[] = [];
+  toggleSearched(val: boolean) {
+    this.hasSearched = val;
+  }
+  
+  mapSelected(data) {
+    this.mapSelect = data;
+  }
+
+  ngAfterViewInit()
+  {
+    this.shortcuts.push
+    (
+      {
+        key: ["up up down down left right left right b a enter"],
+        label: "AdminPanel",
+        description: "AdminTest",
+        command: (output: ShortcutEventOutput) =>
+          console.log("AdminInputTest", output)
+      }
+    );
+  }
+}
+
+
---
+++ app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule, MatAutocompleteModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatPaginatorModule, MatSortModule} from '@angular/material';
+import { NgxLoadingModule, ngxLoadingAnimationTypes  } from 'ngx-loading';
+import { KeyboardShortcutsModule } from "ng-keyboard-shortcuts";
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { SearchformComponent } from './searchform/searchform.component';
+import { MapviewComponent } from './mapview/mapview.component';
+import { TableviewComponent } from './tableview/tableview.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchformComponent,
+    MapviewComponent,
+    TableviewComponent,
+    AdminLoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    KeyboardShortcutsModule,
+    MatTableModule,
+    MatSortModule,
+    MatAutocompleteModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    HttpClientModule,
+    NgxLoadingModule.forRoot({
+      animationType: ngxLoadingAnimationTypes.circleSwish,
+      backdropBackgroundColour: 'rgba(0,0,0,0.5)',
+      backdropBorderRadius: '4px',
+      primaryColour: '#ffffff'
+    }),
+    AppRoutingModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
