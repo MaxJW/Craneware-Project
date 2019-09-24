a3ac2388d182b73e543b21c606af602bc915dcff
CerysK <32930209+CerysK@users.noreply.github.com>
2019-09-24 11:58:45 +0100

    added admin login files

---
+++ src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-login',
+  template: `
+    <h1> Hello </h1>
+    <p>
+      admin-login works!
+    </p>
+  `,
+  styles: []
+})
+export class AdminLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
