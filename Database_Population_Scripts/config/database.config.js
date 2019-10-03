69ca6d0ca424d8bfe2b9e9559d7dd122a62da9d4
Sebastian6656 <46683455+Sebastian6656@users.noreply.github.com>
2019-10-04 00:03:42 +0100

    Added in scripts and config for database for tools used to populate the database, updated results to get in createGeolocationMarker()

---
+++ Database_Population_Scripts/config/database.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    url: "mongodb://dbAdmin:cw123@178.128.47.93:27017/Craneware?authSource=admin",
+    name: "Craneware"
+    // url: "mongodb+srv://Admin:nimdA@industrialteamproject-xo3fn.mongodb.net/Industrial_Team_Project?retryWrites=true&w=majority",
+    // name: "Industrial_Team_Project"
+}
